e5d2a2bc9277372d258e389df03f6d55006ae3af
Vika Trutko <viktrutko@gmail.com>
2019-12-24 18:43:03 +0300

    add test

--- Framework/GitHubAutomation/src/test/java/com/epam/ta/driver/DriverSingleton.java
+++ Framework/GitHubAutomation/src/test/java/com/epam/ta/driver/DriverSingleton.java
@@ -9,18 +9,20 @@ public class DriverSingleton {
 
     private static WebDriver driver;
 
+
     private DriverSingleton(){}
 
     public static WebDriver getDriver(){
         if (null == driver){
             switch (System.getProperty("browser")){
-                case "firefox": {
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
-                }
-                default: {
+                case "chrome": {
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
+
+                }
+                default: {
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
                 }
             }
             driver.manage().window().maximize();
